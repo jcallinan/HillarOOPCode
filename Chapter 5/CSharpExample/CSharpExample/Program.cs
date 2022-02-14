b5a0fb644eca577aac1ff35334dbccf0bd9af980
Jeremy Callinan <jeremy.callinan@gmail.com>
2022-02-13 21:47:03 -0500

    CSharpExample

---
+++ Chapter 5/CSharpExample/CSharpExample/Program.cs
@@ -0,0 +1,11 @@
+﻿using CSharpExample;
+
+
+
+AngryDog angryDog1 = new AngryDog("Brian");
+AngryDog angryDog2 = new AngryDog("Merlin");
+angryDog1.DrawSpeechBalloon("Hello, my name is " + angryDog1.NickName);
+
+
+
+
